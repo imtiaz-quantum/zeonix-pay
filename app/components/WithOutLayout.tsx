200e976c5a2b89c1bb3d70c401ca8347f1db49a0
imtiaz-quantum <imtiaz.quantumdev@gmail.com>
2025-08-21 19:47:44 +0600

    add fixed

--- app/components/WithOutLayout.tsx
+++ app/components/WithOutLayout.tsx
@@ -3,15 +3,13 @@ import { usePathname } from "next/navigation";
 import CustomLayout from "./CustomLayout";
 import { ReactNode } from "react";
 
-export default function WithOutLayout({ children, role, balance }: { children: ReactNode, role?: 'admin' | 'merchant' | 'staff', balance:string }) {
+export default function WithOutLayout({ children, role, balance }: { children: ReactNode, role?: 'admin' | 'merchant' | 'staff', balance: string }) {
   const pathname = usePathname();
-  const isLoginPage = pathname.startsWith('/login');
-  const isHonePage = pathname === ('/');
-  const isServerDownPage = pathname === ('/server-down');
-  const isPaymentPage = pathname === ('/payment');
-  const isSuccessPage = pathname === ('/success');
 
-  
+  // Check if the pathname includes "admin", "merchant", or "staff"
+  const isExcludedPage = pathname.startsWith("/admin") || pathname.startsWith("/merchant") || pathname.startsWith("/staff");
 
-  return isLoginPage || isHonePage || isServerDownPage || isPaymentPage || isSuccessPage? children : <CustomLayout role={role} balance={balance}>{children}</CustomLayout>;
+  return isExcludedPage ? 
+    <CustomLayout role={role} balance={balance}>{children}</CustomLayout> :
+    children;
 }
---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div className="text-center py-20">
+      <h1 className="text-3xl font-bold text-gray-800">404 - Page Not Found</h1>
+      <p className="mt-4 text-lg text-gray-600">Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
